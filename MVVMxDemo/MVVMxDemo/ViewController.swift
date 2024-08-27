bf873391d055dcc4b631efbc60f479765d0415ee
Dipesh <dipesh.mobileappdev@gmail.com>
2024-08-27 21:50:23 +0530

    init commit

---
+++ MVVMxDemo/MVVMxDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMxDemo
+//
+//  Created by Dipesh Makvana on 27/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
